4cce23a98c652c81e10135dcc5f0752aa1be30c5
nvndu <naviduranjuka@gmail.com>
2024-07-27 13:19:50 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import NavBar from './NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ImageSlider from './ImageSlider';
+import Products from './Products';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <ImageSlider/>
+      <Products/>
     </div>
   );
 }
--- src/ImageSlider.jsx
+++ src/ImageSlider.jsx
@@ -4,54 +4,54 @@ import img02 from './Assets/slider/2.jpg';
 import img03 from './Assets/slider/3.jpg';
 import img04 from './Assets/slider/4.jpg';
 import img05 from './Assets/slider/5.jpg';
-// import ExampleCarouselImage from 'components/ExampleCarouselImage';
 
-function ImageSlider() {
+
+function IndividualIntervalsExample() {
   return (
-    <Carousel fade className='corousel-container'>
-      <Carousel.Item interval={1000}>
-        <img src={img01} className='d-block w-100' alt='...' />
+    <Carousel className='corousel-container'>
+      <Carousel.Item interval={2000}>
+        <img src={img01} text="First slide" className='d-block w-100'/>
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item interval={1000}>
-      <img src={img02} className='d-block w-100' alt='...' />
+      <Carousel.Item interval={2000}>
+        <img src={img02} text="Second slide" className='d-block w-100'/>
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item interval={1000}>
-      <img src={img03} className='d-block w-100' alt='...' />
+      <Carousel.Item interval={2000}>
+        <img src={img03} text="Third slide" className='d-block w-100'/>
         <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
           </p>
         </Carousel.Caption>
-      </Carousel.Item >
-      <Carousel.Item interval={1000}>
-      <img src={img04} className='d-block w-100' alt='...' />
+      </Carousel.Item>
+      <Carousel.Item interval={2000}>
+        <img src={img04} text="Fourth slide" className='d-block w-100'/>
         <Carousel.Caption>
-          <h3>4thslide label</h3>
+          <h3>Third slide label</h3>
           <p>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
           </p>
         </Carousel.Caption>
-      </Carousel.Item >
-      <Carousel.Item interval={1000}>
-      <img src={img05} className='d-block w-100' alt='...' />
+      </Carousel.Item>
+      <Carousel.Item interval={2000}>
+        <img src={img05} text="Fifth slide" className='d-block w-100'/>
         <Carousel.Caption>
-          <h3>5th slide label</h3>
+          <h3>Third slide label</h3>
           <p>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
           </p>
         </Carousel.Caption>
-      </Carousel.Item >
+      </Carousel.Item>
     </Carousel>
   );
 }
 
-export default ImageSlider;
+export default IndividualIntervalsExample;
