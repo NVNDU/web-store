ec8dcdd2022a0374fae69a8acca9cfc7b3092bdf
nvndu <naviduranjuka@gmail.com>
2024-08-02 18:10:32 +0530

    added HashRouter

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter,Route,Routes } from 'react-router-dom';
 import Home from './Home';
 import Contact from './Contact';
-import Layout from './Layout';
+
 
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/contact' element={<Contact/>}/>
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
 
